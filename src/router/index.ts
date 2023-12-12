97c8222f0d63a420e91a3f654b4c34f801e06867
zzj <1005978400@qq.com>
2023-12-12 10:56:00 +0800

    新增EditLabel页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,46 +1,46 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Money from '@/views/Money.vue'
-import Label from '@/views/Label.vue'
-import Statistics from '@/views/Statistics.vue'
-import Notfound from '@/views/Notfound.vue'
-import LabelEdit from '@/views/LabelEdit.vue'
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Money from "@/views/Money.vue";
+import Label from "@/views/Label.vue";
+import Statistics from "@/views/Statistics.vue";
+import Notfound from "@/views/Notfound.vue";
+import EditLabel from "@/views/EditLabel.vue";
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    redirect:'/money'
+    path: "/",
+    redirect: "/money",
   },
   {
-    path: '/money',
-    name: 'money',
-    component: Money
+    path: "/money",
+    name: "money",
+    component: Money,
   },
   {
-    path: '/label',
-    name: 'label',
-    component: Label
+    path: "/label",
+    name: "label",
+    component: Label,
   },
   {
-    path: '/statistics',
-    name: 'statistics',
-    component: Statistics
+    path: "/statistics",
+    name: "statistics",
+    component: Statistics,
   },
   {
-    path: 'label/edit',
-    name: 'labelEdit',
-    component:LabelEdit
+    path: "/label/edit/:id",
+    name: "editLabel",
+    component: EditLabel,
   },
   {
-    path: '*',
-    name: 'notfound',
-    component: Notfound
-  }
-]
+    path: "*",
+    name: "notfound",
+    component: Notfound,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/LabelEdit.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Layout />
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
