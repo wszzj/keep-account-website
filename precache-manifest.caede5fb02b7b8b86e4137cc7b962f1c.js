a1acb22f5023fd7f9ea6307a4c5efee9417da244
zzj <1005978400@qq.com>
2023-12-19 12:18:30 +0800

    deploy

---
+++ precache-manifest.caede5fb02b7b8b86e4137cc7b962f1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8033e7986cac16675a",
+    "url": "/keep-account-website/css/app.beb53c95.css"
+  },
+  {
+    "revision": "aab0cb5cae97f3c3ed2e455e96d6b1f8",
+    "url": "/keep-account-website/index.html"
+  },
+  {
+    "revision": "df8033e7986cac16675a",
+    "url": "/keep-account-website/js/app.7365e3e7.js"
+  },
+  {
+    "revision": "17a1b497ac560debed1d",
+    "url": "/keep-account-website/js/chunk-vendors.2860b062.js"
+  },
+  {
+    "revision": "ecfe0d043b7793ac3fe0afa34cdf437f",
+    "url": "/keep-account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keep-account-website/robots.txt"
+  }
+]);
